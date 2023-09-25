b1cabcea9e9b1055f7ad935b388763d1141d5f5e
Murbarkh <murbarkh.abdul_hamid@kcl.ac.uk>
2023-09-25 13:22:31 +0100

    Add primes function

--- primes.py
+++ primes.py
@@ -1,6 +1,32 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
+def has_factor_in_list(num, list):
+    for numbers in list:
+        if num % numbers == 0:
+            return False
+        
+    return True
+
+
+
 def primes(number_of_primes):
-    list = []
+    list = [2]
+
+    if number_of_primes <= 0:
+        raise ValueError
+    
+    num = 3
+    while len(list) != number_of_primes:
+        if has_factor_in_list(num, list):
+            list.append(num)
+
+        num += 1
+
     return list
+
+
+print(primes(10))
+
+
